a75726673e4dd4f235a33fbd6740e7572a986c21
Feihong Hsu <feihong.hsu@gmail.com>
2024-11-12 13:45:48 -0600

    Don't proceed if some screenshots have bad filenames

--- create_pdf.py
+++ create_pdf.py
@@ -7,15 +7,58 @@ import sys
 import fitz
 
 input_dir = Path(sys.argv[1])
-
-sort_fn = lambda d: int(re.match(r'Chapter (\d+)', d.stem).group(1))
-chapters = list(sorted(input_dir.glob('Chapter *'), key=sort_fn))
-
-epilogue = input_dir / 'Epilogue'
-if epilogue.exists():
-  chapters.append(epilogue)
-
 cover_file = next(input_dir.glob('*-cover.png'))
+
+
+def get_chapters():
+  sort_fn = lambda d: int(re.match(r'Chapter (\d+)', d.stem).group(1))
+  chapters = list(sorted(input_dir.glob('Chapter *'), key=sort_fn))
+
+  epilogue = input_dir / 'Epilogue'
+  if epilogue.exists():
+    chapters.append(epilogue)
+
+  references = input_dir / 'References'
+  if references.exists():
+    chapters.append(references)
+
+  return chapters
+
+def validate_screenshots():
+  result = True
+
+  for chapter in get_chapters():
+    for png_file in sorted(chapter.glob('*.png')):
+      if re.match(r'.*\(\d\)$', png_file.stem):
+        print(f'\033[93mInvalid file name: {png_file}')
+        result = False
+
+  return result
+
+def get_screenshot_name(screenshot):
+  return re.match(r'Screenshot_\d{4}-\d{2}-\d{2} (.*) - Art of Problem Solving', screenshot.stem).group(1)
+
+def get_screenshots(chapter):
+  """
+  Get screenshots for the given chapter in the correct order
+  """
+  # Make sure review problems come before challenge problems
+  def sort_fn(f):
+    name = get_screenshot_name(f)
+    if name.startswith('Review Problems'):
+      return 'A' + name
+    elif name.startswith('Challenge Problems'):
+      return 'B' + name
+    else:
+      return name
+
+  screenshots = list(chapter.glob('*.png'))
+  screenshots.sort(key=sort_fn)
+
+  return screenshots
+
+if not validate_screenshots():
+  sys.exit(1)
 
 out_file = Path('output.pdf')
 if out_file.exists():
@@ -27,12 +70,11 @@ doc.insert_file(cover_file)
 curr_page = 2
 toc = []
 
-for chapter in chapters:
+for chapter in get_chapters():
   toc.append([1, chapter.name, curr_page])
 
-  for png_file in sorted(chapter.glob('*.png')):
-    m = re.match(r'Screenshot_\d{4}-\d{2}-\d{2} (.*) - Art of Problem Solving', png_file.stem)
-    page_name = m.group(1)
+  for png_file in get_screenshots(chapter):
+    page_name = get_screenshot_name(png_file)
     print(f'{chapter.name}: {page_name}')
     doc.insert_file(png_file)
     toc.append([2, page_name, curr_page])
