c296e2a4e28b93d4e51a46288f1731bfae4115ab
Feihong Hsu <feihong.hsu@gmail.com>
2025-05-18 19:59:24 -0500

    Add olympiad.asy for convenience

--- README.md
+++ README.md
@@ -20,12 +20,13 @@ Stitch homework PDF files inside `homework/` directory into a single PDF file:
 
 ## TODO
 
+- Page for rendering jsut a single asymptote figure
 - Click on problem description to black out all other elements on the page
 - Generate an TOC for all problems on a page
-- Make page to debug individual asymptote figures
 
 ## Links
 
 - [TinyTeX](https://github.com/rstudio/tinytex)
 - [Asymptote: The Vector Graphics Language](https://asymptote.sourceforge.io/)
 - [Asymptote geometry module](https://web.archive.org/web/20200128180233/http://www.piprime.fr/files/asymptote/geometry/modules/geometry.asy.index.type.html)
+- [Asymptote: Macros and Packages](https://artofproblemsolving.com/wiki/index.php/Asymptote:_Macros_and_Packages)
--- figure.py
+++ figure.py
@@ -3,10 +3,12 @@ Render asymptote code into svg files
 """
 from pathlib import Path
 import subprocess
+import os
 import hashlib
 
 
 figures_dir = Path('figures')
+asymptote_dir = Path('asymptote_dir').absolute()
 
 
 def get_hash(code):
@@ -20,10 +22,12 @@ def get_asy_file(code):
    return figures_dir / f'{get_hash(code)}.asy'
 
 def generate_svg_files(asy_files):
+    os.environ['ASYMPTOTE_DIR'] = str(asymptote_dir)
     cmd = [
         'asy',
         '-outformat', 'svg',
         *(f.name for f in asy_files),
     ]
     print(cmd)
-    return subprocess.run(cmd, cwd=figures_dir, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')
+    return subprocess.run(cmd, cwd=figures_dir, env=os.environ, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                          encoding='utf-8')
