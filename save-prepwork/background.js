b5dd1147aa499144ccf2ecb8837dc276300b23b9
Feihong Hsu <feihong.hsu@gmail.com>
2026-04-11 16:23:07 -0500

    Simplify chrome extension logic

--- save-prepwork/background.js
+++ save-prepwork/background.js
@@ -1,18 +1,9 @@
-// https://developer.chrome.com/docs/extensions/reference/api/windows
-
-const WINDOW_WIDTH = 1000
-
-const getImageName = title => {
-  // Extract week number
-  let numStr = title.match(/Week (\d)+/)[1]
-  numStr = numStr.padStart(2, '0')
-  return `prepwork ${numStr}.png`
-}
+const WINDOW_WIDTH = 800
 
 async function start(tab) {
   const current = await chrome.windows.getCurrent()
 
-  // Set the width of window to a specific value for consistency in captured images
+  // Set the width of window to a specific value for consistency in PDFs
   await chrome.windows.update(current.id, {
     width: WINDOW_WIDTH,
     state: 'normal',
@@ -21,25 +12,8 @@ async function start(tab) {
   // Execute content.js in the context of the page
   await chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    files: [ 'libs/html2canvas.min.js', 'content.js' ],
+    files: [ 'content.js' ],
   })
 }
 
-// Handle image data sent from content script
-async function handleMessage(message, sender, sendResponse) {
-  if (message.title && message.dataUrl) {
-    // Download the image
-    const downloadId = await chrome.downloads.download({
-      url: message.dataUrl,
-      filename: getImageName(message.title),
-      conflictAction: 'overwrite',
-      saveAs: false // Set to true to show the "Save As" dialog
-    })
-    console.log('Download ID:', downloadId)
-  }
-
-  return true
-}
-
 chrome.action.onClicked.addListener(start)
-chrome.runtime.onMessage.addListener(handleMessage)
--- save-prepwork/content.js
+++ save-prepwork/content.js
@@ -1,42 +1,20 @@
 /*
 Script to execute inside page
 */
+{
+  const ELEMENT_TO_LIFT = '.aops-scrollbar-not-visible'
 
-const ELEMENT_TO_SCREENSHOT = '.aops-scrollbar-not-visible'
+  async function main() {
+    const pathname = window.location.pathname
+    if (pathname.includes('/prepwork/')) {
+      // Get rid of UI other than the prep work content
+      let title = document.querySelector('h3.title').textContent
+      document.body.innerHTML = document.querySelector(ELEMENT_TO_LIFT).innerHTML
+      const h1 = document.createElement('h1')
+      h1.innerHTML = title
+      document.body.prepend(h1)
+    }
+  }
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
-const getBlob = canvas => new Promise(resolve => canvas.toBlob(resolve))
-
-const getBase64String = blob => new Promise(resolve => {
-  const reader = new FileReader()
-  reader.onloadend = () => resolve(reader.result)
-  reader.readAsDataURL(blob)
-})
-
-async function main() {
-  // Temporarily get rid of UI other than the prep work content
-  let originalContent = document.body.innerHTML
-  let title = document.querySelector('h3.title').textContent
-  document.body.innerHTML = document.querySelector(ELEMENT_TO_SCREENSHOT).innerHTML
-  const h1 = document.createElement('h1')
-  h1.innerHTML = title
-  document.body.prepend(h1)
-
-  await sleep(100)
-
-  // Get canvas from document body
-  const canvas = await html2canvas(document.body)
-  const blob = await getBlob(canvas)
-  console.log(blob)
-
-  const dataUrl = await getBase64String(blob)
-
-  await chrome.runtime.sendMessage({ title, dataUrl })
-  console.log('Sent image data back to service worker')
-
-  // Restore page to original state
-  // document.body.innerHTML = originalContent
+  main()
 }
-
-main()
