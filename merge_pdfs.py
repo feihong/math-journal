044b79e74ed329f5359de14baf733df4d888bf32
Feihong Hsu <feihong.hsu@gmail.com>
2026-04-12 12:38:35 -0500

    Handle arbitrary ordering of files within a week

--- merge_pdfs.py
+++ merge_pdfs.py
@@ -4,29 +4,57 @@ Merge all homework PDFs into a single PDF that has a table of contents
 Reference: https://github.com/pymupdf/PyMuPDF-Utilities/blob/master/examples/import-toc/import.py
 
 """
+import re
 from pathlib import Path
+import functools
+import typing
 import pymupdf
 
-output_file = Path('homework.pdf')
-homework_dir = Path('homework')
+output_file = Path('AoPS Intermediate Algebra Homework.pdf')
+homework_dir = Path('~/Downloads/aops intermediate algebra').expanduser()
+
+# Specifies relative ordering of files within a week as well as what their titles are in the TOC
+file_order_title = [
+  ('board', 'Message Board Problems'),
+  ('intalg', 'Homework'),
+  ('solution', 'Homework Solutions')
+]
+
+
+title_map = dict(file_order_title)
+order_map = dict((p[0], i) for i, p in enumerate(file_order_title))
+
+
+@functools.total_ordering
+class PdfFile(typing.NamedTuple):
+  filename: str
+  name: str
+  num: int
+
+  @staticmethod
+  def make(filename):
+    name, num = re.match(r'([a-z]+) (\d{2})\.pdf', filename).groups()
+    return PdfFile(filename=filename, name=name, num=int(num))
+
+  @property
+  def title(self):
+    title_suffix = title_map[self.name]
+    return f'Week {self.num} {title_suffix}'
+
+  @property
+  def path(self):
+    return homework_dir / self.filename
+
+  def __eq__(self, other):
+    return (self.num, self.name) == (other.num, other.name)
+
+  def __lt__(self, other):
+    return (self.num, order_map[self.name]) < (other.num, order_map[other.name])
 
 
 def main():
-  # Copy all homework ##.pdf files into this project's homework directory
-  downloaded_files = Path('~/Downloads').expanduser().glob('homework ??.pdf')
-  for downloaded_file in downloaded_files:
-    dest = homework_dir / downloaded_file.name
-    downloaded_file.replace(dest)
-    print(f'Move {downloaded_file} to {dest}')
-  print(list(downloaded_files))
-
-  # Treat the number at the end of filename as an int instead of a string
-  def key_function(path: Path):
-    name, num = path.stem.rsplit(' ', maxsplit=1)
-    return (name, int(num))
-
-  # Get all PDF files in order
-  pdf_files = list(sorted(homework_dir.glob('*.pdf'), key=key_function))
+  # Get all PDF files in the correct order
+  pdf_files = list(sorted(PdfFile.make(f.name) for f in homework_dir.glob('*.pdf')))
   merge(pdf_files)
 
 
@@ -36,11 +64,11 @@ def merge(pdf_files):
   curr_page = 1
   toc = []
 
-  for i, pdf_file in enumerate(pdf_files, 1):
-    new_doc = pymupdf.open(pdf_file)
+  for pdf_file in pdf_files:
+    new_doc = pymupdf.open(pdf_file.path)
     doc.insert_pdf(new_doc)
 
-    toc_item = [1, f'Homework {i}', curr_page]
+    toc_item = [1, pdf_file.title, curr_page]
     print(toc_item)
     toc.append(toc_item)
 
@@ -49,7 +77,8 @@ def merge(pdf_files):
   doc.set_toc(toc)
 
   doc.save(output_file)
-  print(f'Saved {output_file}')
+  print(f'Saved {output_file.absolute()}')
+
 
 if __name__ == '__main__':
   main()
