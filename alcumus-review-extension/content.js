1f27f306d41d9a8a18ff60dfba2924c344115f5c
Feihong Hsu <feihong.hsu@gmail.com>
2026-08-07 21:03:10 -0500

    Hide solution even when switching to different past problem

--- alcumus-review-extension/content.js
+++ alcumus-review-extension/content.js
@@ -1,35 +1,64 @@
 const key = 'isActivated'
 
-const observer = new MutationObserver((_mutations, obs) => {
-  const target = document.querySelector('.alc-solution-box')
+// This justs improves UI, so no need to use mutation observer. Small flicker is acceptable.
+setTimeout(() => {
+  const logPanel = document.querySelector('.alc-log-panel .aops-scroll-outer')
 
-  if (target) {
-    target.style.visibility = 'hidden'
+  // Make Log panel taller
+  logPanel.style.height = '500px'
 
-    // Add button to show the solution
-    const btn = document.createElement('button')
-    btn.innerHTML = 'Show solution'
-    btn.addEventListener('click', () => target.style.visibility = 'visible')
-    target.before(btn)
+  // Hide tall ad banner
+  const banner = document.querySelector('.ready-for-next')
+  banner.style.display = 'none'
 
-    // Hide tall ad banner
-    const banner = document.querySelector('.ready-for-next')
-    console.log(banner)
-    banner.style.display = 'none'
-
-    // Make Log panel taller
-    const logPanel = document.querySelector('.alc-log-panel .aops-scroll-outer')
+  const observer = new MutationObserver(() => {
+    // console.log('Adjust height')
     logPanel.style.height = '500px'
+  })
+  observer.observe(logPanel, { attributes: true })
+}, 500)
 
-    obs.disconnect()
-  }
-})
+function hideSolution() {
+
+  const observer = new MutationObserver((_mutations, obs) => {
+    const target = document.querySelector('.alc-solution-box')
+    if (target) {
+      if (target.style.visibility === 'hidden') return
+
+      target.style.visibility = 'hidden'
+
+      // Add button to show the solution
+      const btn = document.createElement('button')
+      btn.innerHTML = 'Show solution'
+      btn.addEventListener('click', () => target.style.visibility = 'visible')
+      target.before(btn)
+
+      obs.disconnect()
+    }
+  })
+  observer.observe(document.body, { childList: true, subtree: true })
+}
+
+function isPastProblemUrl(url) {
+  return url.match(/\/alcumus\/report\/me\/trial\/.*/) !== null
+}
 
 async function main() {
-  const result = await chrome.storage.local.get([key])
-  isActivated = !!result.isActivated
-  if (isActivated) {
-    observer.observe(document.body, { childList: true, subtree: true })
+  if (isPastProblemUrl(location.pathname)) {
+    console.log(location.href)
+
+    const result = await chrome.storage.local.get([key])
+    if (!result.isActivated) return
+
+    hideSolution()
+
+    window.navigation.addEventListener("navigate", (event) => {
+      hideSolution()
+      const url = event.destination.url
+      if (isPastProblemUrl(url)) {
+        console.log(url)
+      }
+    })
   }
 }
 
--- alcumus-review-extension/popup.js
+++ alcumus-review-extension/popup.js
@@ -3,21 +3,15 @@ const key = 'isActivated'
 const btn = document.querySelector('button')
 let isActivated = undefined
 
-function update() {
-  btn.innerHTML = isActivated ? "Disable" : "Enable"
-}
-
 async function main() {
   const result = await chrome.storage.local.get([key])
   isActivated = !!result.isActivated
-  update()
+  btn.innerHTML = isActivated ? "Disable" : "Enable"
 
   btn.addEventListener('click', async () => {
     isActivated = !isActivated
     await chrome.storage.local.set({ [key]: isActivated })
-    update()
-
-    setTimeout(() => window.close(), 500)
+    window.close()
   })
 }
 
